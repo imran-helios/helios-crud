da5b08f52dceaa0f2df9615e5394002e909c470f
imran-helios <imran.mridha@heliosinsider.com>
2023-01-28 13:21:47 +0600

    Added Form Fields

--- crud/models.py
+++ crud/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-# class Registration(models.Model):
+class Registration(models.Model):
+    name = models.CharField(max_length=70)
+    phone = models.CharField(max_length = 100)
